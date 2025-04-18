978e856a37bf22b5b48f17cfb3e42a2768c6a45b
Jon Seymour <jon@upowr.com.au>
2025-04-19 09:57:03 +1000

    force a linux/amd64 build

--- build-and-test.sh
+++ build-and-test.sh
@@ -3,11 +3,11 @@ set -e
 VERSION=$(cat VERSION)
 
 INTERACTIVE=$(if test -t 0; then echo -i; fi)
-docker build . -t developmentseed/geolambda:${VERSION}
+docker build --platform linux/amd64 . -t developmentseed/geolambda:${VERSION}
 docker run --rm -v $PWD:/home/geolambda ${INTERACTIVE} -t developmentseed/geolambda:${VERSION} package.sh
 
 cd python
-docker build . --build-arg VERSION=${VERSION} -t developmentseed/geolambda:${VERSION}-python
+docker build . -platform linux/amd64 --build-arg VERSION=${VERSION} -t developmentseed/geolambda:${VERSION}-python
 docker run -v ${PWD}:/home/geolambda -t developmentseed/geolambda:${VERSION}-python package-python.sh
 
 docker run --rm -e PROJ_LIB=/opt/share/proj -v ${PWD}/lambda:/var/task -v ${PWD}/../lambda:/opt lambci/lambda:python3.7 lambda_function.lambda_handler '{}'
