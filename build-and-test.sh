c9773a981efb89ecc069c9b3d7937d29c648cea2
Jon Seymour <jon@upowr.com.au>
2025-04-19 14:18:01 +1000

    lambda/python/later/geolamba: add support for depot builds

--- build-and-test.sh
+++ build-and-test.sh
@@ -3,11 +3,17 @@ set -e
 VERSION=$(cat VERSION)
 
 INTERACTIVE=$(if test -t 0; then echo -i; fi)
-docker build --platform linux/amd64 . -t developmentseed/geolambda:${VERSION}
+
+declare -a depot_opts=()
+if build.sh get depot-installed >/dev/null; then
+	depot_opts=(--load)
+fi
+
+docker build --platform linux/amd64 . -t developmentseed/geolambda:${VERSION} "${depot_opts[@]}"
 docker run --rm -v $PWD:/home/geolambda ${INTERACTIVE} -t developmentseed/geolambda:${VERSION} package.sh
 
 cd python
-docker build . -platform linux/amd64 --build-arg VERSION=${VERSION} -t developmentseed/geolambda:${VERSION}-python
+docker build . --platform linux/amd64 --build-arg VERSION=${VERSION} -t developmentseed/geolambda:${VERSION}-python "${depot_opts[@]}"
 docker run -v ${PWD}:/home/geolambda -t developmentseed/geolambda:${VERSION}-python package-python.sh
 
 docker run --rm -e PROJ_LIB=/opt/share/proj -v ${PWD}/lambda:/var/task -v ${PWD}/../lambda:/opt lambci/lambda:python3.7 lambda_function.lambda_handler '{}'
